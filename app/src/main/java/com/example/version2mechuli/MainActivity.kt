6f6451a99f34a6e3f72c41afe669420167fcc95a
Yuhee Kim <79076150+hapen7535@users.noreply.github.com>
2022-04-12 22:22:40 +0900

    design : 메인 화면 UI 작성

--- app/src/main/java/com/example/version2mechuli/MainActivity.kt
+++ app/src/main/java/com/example/version2mechuli/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.version2mechuli
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var startBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startBtn = findViewById<Button>(R.id.startbtn)
+
+        title = "메추리"
+
+        startBtn.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+
+
     }
 }
