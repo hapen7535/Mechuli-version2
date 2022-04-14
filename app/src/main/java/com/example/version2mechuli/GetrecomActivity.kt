f46791ec3fe56d0912021002df157d3053e82d8c
Yuhee Kim <79076150+hapen7535@users.noreply.github.com>
2022-04-14 20:27:59 +0900

    design : 메뉴 분류 태그 추가

--- app/src/main/java/com/example/version2mechuli/GetrecomActivity.kt
+++ app/src/main/java/com/example/version2mechuli/GetrecomActivity.kt
@@ -2,12 +2,14 @@ package com.example.version2mechuli
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.version2mechuli.databinding.ActivityGetrecomBinding
 import com.example.version2mechuli.databinding.ActivityLoginBinding
 
 class GetrecomActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityGetrecomBinding
+    lateinit var arrMenu : ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +22,26 @@ class GetrecomActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        arrMenu = arrayListOf("떡볶이","파스타","비빔밥","라멘","아이스크림")
+        addMenuView(arrMenu)
+
     }
+
+    private fun addMenuView(arr : ArrayList<String>){
+
+        arr.forEach{
+
+            val menuView = layoutInflater.inflate(R.layout.menutest_layout, null, false)
+            val menuViewText = menuView.findViewById<TextView>(R.id.menuName)
+
+            menuViewText.setText(it)
+            binding.addMenu.addView(menuView)
+
+            menuView.setOnClickListener {
+                //tagView.setBackgroundResource(R.drawable.taglayout);
+            }
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/version2mechuli/RandomActivity.kt
+++ app/src/main/java/com/example/version2mechuli/RandomActivity.kt
@@ -2,12 +2,48 @@ package com.example.version2mechuli
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.version2mechuli.databinding.ActivityLoginBinding
+import com.example.version2mechuli.databinding.ActivityRandomBinding
 
 class RandomActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityRandomBinding
+    lateinit var arrTag : ArrayList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_random)
 
+        binding = ActivityRandomBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+            //arrTag = arrayListOf("전체","분식","한식","양식","중식")
+            //addTagView(arrTag)
+
+        binding.returnbtn.setOnClickListener {
+            onBackPressed()
+        }
+
 
     }
+
+    private fun addTagView(arr : ArrayList<String>){
+
+
+        arr.forEach{
+
+            val tagView = layoutInflater.inflate(R.layout.tags, null, false)
+            val tagViewText = tagView.findViewById<TextView>(R.id.tagRadio)
+
+            tagViewText.setText(it)
+            binding.addTag.addView(tagView)
+
+            tagView.setOnClickListener {
+                //tagView.setBackgroundResource(R.drawable.taglayout);
+            }
+        }
+
+    }
+
 }
