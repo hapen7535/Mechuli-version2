755090fc02e000d427fbd9013e920004438afa41
Yuhee Kim <79076150+hapen7535@users.noreply.github.com>
2022-04-14 21:35:12 +0900

    fix : 평가이력 rating 안되는 문제 수정

--- app/src/main/java/com/example/version2mechuli/GetrecomActivity.kt
+++ app/src/main/java/com/example/version2mechuli/GetrecomActivity.kt
@@ -9,7 +9,6 @@ import com.example.version2mechuli.databinding.ActivityLoginBinding
 class GetrecomActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityGetrecomBinding
-    lateinit var arrMenu : ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,26 +21,8 @@ class GetrecomActivity : AppCompatActivity() {
             onBackPressed()
         }
 
-        arrMenu = arrayListOf("떡볶이","파스타","비빔밥","라멘","아이스크림")
-        addMenuView(arrMenu)
 
     }
 
-    private fun addMenuView(arr : ArrayList<String>){
-
-        arr.forEach{
-
-            val menuView = layoutInflater.inflate(R.layout.menutest_layout, null, false)
-            val menuViewText = menuView.findViewById<TextView>(R.id.menuName)
-
-            menuViewText.setText(it)
-            binding.addMenu.addView(menuView)
-
-            menuView.setOnClickListener {
-                //tagView.setBackgroundResource(R.drawable.taglayout);
-            }
-        }
-
-    }
 
 }
--- app/src/main/java/com/example/version2mechuli/SigndataActivity.kt
+++ app/src/main/java/com/example/version2mechuli/SigndataActivity.kt
@@ -2,12 +2,14 @@ package com.example.version2mechuli
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.version2mechuli.databinding.ActivityLoginBinding
 import com.example.version2mechuli.databinding.ActivitySigndataBinding
 
 class SigndataActivity : AppCompatActivity() {
 
     lateinit var binding : ActivitySigndataBinding
+    lateinit var arrMenu : ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -23,5 +25,26 @@ class SigndataActivity : AppCompatActivity() {
 
         }
 
+        arrMenu = arrayListOf("떡볶이","파스타","비빔밥","라멘","아이스크림")
+        addMenuView(arrMenu)
+
     }
+
+    private fun addMenuView(arr : ArrayList<String>){
+
+        arr.forEach{
+
+            val menuView = layoutInflater.inflate(R.layout.menutest_layout, null, false)
+            val menuViewText = menuView.findViewById<TextView>(R.id.menuName)
+
+            menuViewText.setText(it)
+            binding.addMenu.addView(menuView)
+
+            menuView.setOnClickListener {
+                //tagView.setBackgroundResource(R.drawable.taglayout);
+            }
+        }
+
+    }
+
 }
