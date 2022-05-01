f61455fecc95620c5886da4494bf9ad7511ab19a
Yuhee Kim <dbgml4509@naver.com>
2022-05-01 13:08:12 +0900

    fix : 네트워크 응답 비동기로 처리

---
+++ app/src/main/java/com/example/version2mechuli/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.version2mechuli
+
+class Constants {
+
+    companion object{
+        const val BASE_URL = "http://10.0.2.2:3333/"
+    }
+
+}
--- app/src/main/java/com/example/version2mechuli/SignupActivity.kt
+++ app/src/main/java/com/example/version2mechuli/SignupActivity.kt
@@ -2,12 +2,16 @@ package com.example.version2mechuli
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.version2mechuli.databinding.ActivitySignupBinding
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import retrofit2.Call
 import retrofit2.Response
@@ -18,6 +22,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 class SignupActivity : AppCompatActivity() {
 
     lateinit var binding : ActivitySignupBinding
+    private val idch by lazy { findViewById<EditText>(R.id.setID)}
+    private val checkPoint by lazy { findViewById<TextView>(R.id.checkpoint) }
+    private val callList = mutableListOf<Call<*>>()
+    var check : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -30,6 +38,32 @@ class SignupActivity : AppCompatActivity() {
         var userPw = binding.setPW.text
         var duplicatePw = binding.dupPw.text
 
+        /*
+        binding.dupChk.setOnClickListener {
+            var id = binding.setID.text.toString()
+
+
+                /*
+                //중복되는 아이디라면
+                if (check == false) {
+                    if(binding.noneDup.visibility == View.INVISIBLE) {
+                        binding.dupText.visibility = View.VISIBLE
+                    }
+                    binding.signStart.isEnabled = false
+                }
+
+                //중복되지 않는 아이디라면
+                if (check == true) {
+                    if(binding.dupText.visibility == View.INVISIBLE) {
+                        binding.noneDup.visibility = View.VISIBLE
+                    }
+                    binding.signStart.isEnabled = true
+                }
+
+                 */
+
+
+        }*/
 
         binding.signStart.setOnClickListener{
 
@@ -52,56 +86,86 @@ class SignupActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+
     }
 
-    fun checkBtn(view : View) {
-        var id = binding.setID.text.toString()
+    fun dupId(view : View) {
+        val id = idch.text.toString()
+        val call = InfoClient.service.requestData(id)
+        call.enqueue(object : Callback<GetIdresult>{
+            override fun onFailure(call: Call<GetIdresult>, t: Throwable) {
+                Toast.makeText(
+                    applicationContext,
+                    "통신 실패 : " + t.message,
+                    Toast.LENGTH_LONG
+                    ).show()
+                Log.d("myTag", (t.message.toString()))
+            }
 
-        //중복되는 아이디라면
-        if (checkId(id) == false) {
-            binding.dupText.visibility = View.VISIBLE
-            binding.signStart.isEnabled = false
-        }
+            override fun onResponse(call: Call<GetIdresult>, response: Response<GetIdresult>) {
+                val body = response.body()
+                if(body != null && response.isSuccessful){
+                    checkPoint.text = Gson().toJson(body)
+                }
+            }
+        })
+        callList.add(call)
 
-        //중복되지 않는 아이디라면
-        if (checkId(id) == true) {
-            binding.noneDup.visibility = View.VISIBLE
-        }
     }
 
-    private fun checkId(id: String): Boolean {
+    override fun onDestroy() { //Activity 종료 후 네트워크 요청 취소
+        callList.forEach { it.cancel() }
+        super.onDestroy()
+    }
+
+    suspend fun checkId(id: String) : Boolean {
         var gson = GsonBuilder().setLenient().create()
-        var checkBool: Boolean = false
         var retrofit = Retrofit.Builder() //레트로핏 인스턴스 생성
             .baseUrl("http://10.0.2.2:3333/")
             .addConverterFactory(GsonConverterFactory.create(gson)) // 파싱등록
             .build()
 
-        if (id != null) {
+//        return suspendCancellableCoroutine { cont ->
+            if (id != null) {
 
-    //            Toast.makeText(applicationContext, "id : " + id + ", pw : " + pw, Toast.LENGTH_LONG).show()
-            var sendUserid: SendUserId = retrofit.create(SendUserId::class.java) //아이디 전송
+                //            Toast.makeText(applicationContext, "id : " + id + ", pw : " + pw, Toast.LENGTH_LONG).show()
+                var sendUserid: SendUserId = retrofit.create(SendUserId::class.java) //아이디 전송
 
-            sendUserid.requestData(id).enqueue(object : Callback<GetIdresult> {
-                override fun onFailure(call: Call<GetIdresult>, t: Throwable) { //통신 실패
-                    Toast.makeText(applicationContext, "통신 실패 : " + t.message, Toast.LENGTH_LONG)
-                        .show()
-                    Log.d("myTag", (t.message.toString()))
-    //                    Log.d("error", (t.message.toString()))
-                }
+                sendUserid.requestData(id).enqueue(object : Callback<GetIdresult> {
+                    override fun onFailure(call: Call<GetIdresult>, t: Throwable) { //통신 실패
+                        Toast.makeText(
+                            applicationContext,
+                            "통신 실패 : " + t.message,
+                            Toast.LENGTH_LONG
+                        )
+                            .show()
+                        Log.d("myTag", (t.message.toString()))
+//                        cont.resumeWithException(t)
 
-                override fun onResponse(
-                    call: Call<GetIdresult>,
-                    response: Response<GetIdresult>
-                ) { //통신 성공
-                    checkBool = response.body()!!.isRedup
-                    Log.d("myTag", "res : " + checkBool) //중복되는지 결과
-    //                    Log.d("id,pw",(arr?.userId + arr?.password))
-                }
-            })
-        }
+                    }
 
-        return checkBool
+                    override fun onResponse(
+                        call: Call<GetIdresult>,
+                        response: Response<GetIdresult>
+                    ) { //통신 성공
+                        if (response.isSuccessful) {
+                            var dataclass = response.body() //중복되는지 결과
+                            check = dataclass!!.isRedup
+                            Log.d("Idcheck", check.toString())
+//                            cont.resume(response.body()!!.isRedup)
+                        } else {
+//                            cont.resumeWithException(Exception("Received error response : ${response.message()}"))
+                        }
+
+                    }
+
+                })
+
+
+            }
+
+        return check
+
     }
 
 
@@ -117,7 +181,7 @@ class SignupActivity : AppCompatActivity() {
             .build()
 
 
-        if(id != null && pw != null && !checkId(id)) {
+        if(id != null && pw != null ) {
 
 //            Toast.makeText(applicationContext, "id : " + id + ", pw : " + pw, Toast.LENGTH_LONG).show()
             var sendUserdata: SendUserdata = retrofit.create(SendUserdata::class.java) //아이디, 비밀번호 전송
@@ -143,4 +207,5 @@ class SignupActivity : AppCompatActivity() {
 
 
     }
+
 
