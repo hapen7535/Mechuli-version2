eaf5bb39bc308868739941301fb0437ef8b3362a
Yuhee Kim <dbgml4509@naver.com>
2022-06-03 15:07:49 +0900

    feat : 회원가입 평가이력 이미지 적용

--- app/src/main/java/com/example/version2mechuli/SigndataActivity.kt
+++ app/src/main/java/com/example/version2mechuli/SigndataActivity.kt
@@ -2,13 +2,16 @@ package com.example.version2mechuli
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
 import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
 //import com.bumptech.glide.Glide
 import com.example.version2mechuli.databinding.ActivitySigndataBinding
 import com.google.gson.Gson
@@ -23,31 +26,29 @@ class SigndataActivity : AppCompatActivity() {
     lateinit var arrMenu : ArrayList<String>
     lateinit var userid : String
     lateinit var userpw : String
-    lateinit var imgList : ArrayList<String>
 
     var ratingList = mutableMapOf<String, Float>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signdata)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_signdata)
 
-
+        var imgList = arrayListOf(binding.menuImg1, binding.menuImg2, binding.menuImg3, binding.menuImg4, binding.menuImg5)
+        var textList = arrayListOf(binding.menuName1, binding.menuName2, binding.menuName3, binding.menuName4, binding.menuName5)
         val secondIntent = getIntent()
         userid = secondIntent.getStringExtra("id").toString()
         userpw = secondIntent.getStringExtra("pw").toString()
         arrMenu = arrayListOf("떡볶이","파스타","쌈밥","라멘","와플")
 
-        getMenuImg(arrMenu)
+        getMenuImg(arrMenu, imgList, textList)
 
-        binding = ActivitySigndataBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+//        binding = ActivitySigndataBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
 
         binding.returnbtn.setOnClickListener{
             onBackPressed()
         }
-
-        addMenuView(arrMenu)
 
         binding.signTest.setOnClickListener {
             if(ratingList.size < 5){
@@ -59,30 +60,45 @@ class SigndataActivity : AppCompatActivity() {
         }
     }
 
-    private fun addMenuView(arr : ArrayList<String>){
+    private fun menuSetting(arr: ArrayList<String>){
 
+            binding.menuName1.setText(arr[0])
 
-        arr.forEach{
-
-            val menuView = layoutInflater.inflate(R.layout.menutest_layout, null, false)
-            val menuViewText = menuView.findViewById<TextView>(R.id.menuName)
-            val menuRating = menuView.findViewById<RatingBar>(R.id.ratingBar)
-            val rating = menuRating.rating
-
-            menuViewText.setText(it)
-            binding.addMenu.addView(menuView)
-
-            menuRating.setOnRatingBarChangeListener{ ratingBar, rating, fromUser->
-                ratingBar.rating
-                ratingList[it] = rating
-                Log.d("ratingList", ratingList.toString())
-            }
-        }
     }
 
-    private fun getMenuImg(nameList : ArrayList<String>){
 
-        val imgList  = arrayListOf<String>()
+//    private fun addMenuView(arr : ArrayList<String>, imgRsc : ArrayList<String>){
+//
+//        var i = 0
+//        arr.forEach{
+//
+//            val menuView = layoutInflater.inflate(R.layout.menutest_layout, null, false)
+//            val menuViewText = menuView.findViewById<TextView>(R.id.menuName)
+//            val menuRating = menuView.findViewById<RatingBar>(R.id.ratingBar)
+//            val menuImg = menuView.findViewById<ImageView>(R.id.menuImg)
+//            val rating = menuRating.rating
+//
+//            menuViewText.setText(it)
+//            Glide.with(this)
+//                .load(imgRsc[i])
+//                .into(menuImg)
+//            binding.addMenu.addView(menuView)
+//
+//            menuRating.setOnRatingBarChangeListener{ ratingBar, rating, fromUser->
+//                ratingBar.rating
+//                ratingList[it] = rating
+//                Log.d("ratingList", ratingList.toString())
+//            }
+//
+//            i += 1
+//
+//        }
+//    }
+
+    private fun getMenuImg(nameList : ArrayList<String>, imgLayout : ArrayList<ImageView>,textlayout : ArrayList<TextView>){
+
+        var i = 0
+
             lifecycleScope.launch {
                 Log.d("myTag", "서버 요청 실행")
                 //UI
@@ -90,6 +106,20 @@ class SigndataActivity : AppCompatActivity() {
                     InfoClientMenuImg.service.requestData(nameList)
                 }
                 //UI
+
+                val answer = res.resultList
+//                this@SigndataActivity.imgList = answer.map{ it.image1 } as ArrayList<String>
+//
+//                addMenuView(arrMenu, imgList)
+
+                answer.forEach {
+                    textlayout[i].setText(it.foodName)
+                    Glide.with(this@SigndataActivity)
+                        .load(it.image2)
+                        .into(imgLayout[i])
+                    i += 1
+                }
+
                 Log.d("myTag", "서버 데이터 받음 : " + res)
                 Log.d("myTag", "0 : " + res.resultList[0])
                 Log.d("myTag", "1 : " + res.resultList[1])
