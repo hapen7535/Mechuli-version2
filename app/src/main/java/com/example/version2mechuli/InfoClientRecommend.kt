4fc5b3e632c7d1fb6767667fe953163f0da0f597
Yuhee Kim <dbgml4509@naver.com>
2022-06-06 22:47:27 +0900

    feat : 회원 추천 부분 타임아웃 추가

--- app/src/main/java/com/example/version2mechuli/InfoClientRecommend.kt
+++ app/src/main/java/com/example/version2mechuli/InfoClientRecommend.kt
@@ -1,14 +1,25 @@
 package com.example.version2mechuli
 
 import com.example.version2mechuli.Constants.Companion.BASE_URL
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object InfoClientRecommend {
+
+    private val okHttpClient by lazy {
+        OkHttpClient.Builder()
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .readTimeout(1, TimeUnit.MINUTES)
+            .writeTimeout(1, TimeUnit.MINUTES)
+            .build()
+    }
 
     private val retrofit by lazy{ //by lazy로 인해 api 변수가 사용될 때 초기화될 수 있다.
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
